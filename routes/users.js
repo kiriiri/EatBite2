b8cc022315071f0842120779bb12d9aca2a88819
Stacy Kiriiri <Wkiriiri@gmail.com>
2021-09-15 11:17:51 +0300

    add json files

--- routes/users.js
+++ routes/users.js
@@ -2,9 +2,28 @@ import express from 'express';
 
 const router = express.Router();
 
+const users = [
+    {
+        firstName: "John",
+        lastName: "Doe",
+        age: 25
+    },
+    {
+        firstName: "Jane",
+        lastName: "Doe",
+        age: 24
+    }
+]
+
 // All routes in here are starting with /users //
 router.get('/', (req, res) => {
-    res.send('Hello');
+    console.log(users);
+
+    res.send(users);
+});
+
+router.post('/', (req, res) => {
+    
 });
 
 export default router;
